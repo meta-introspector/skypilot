c3f1d3e4f9f5335f1d19ccb40005fa96bb0a5802
mike dupont <mike.dupont@introspector.local>
2024-12-10 16:26:36 -0500

    created second cluster
    currently you need to run the second step twice because pydantic gets reinstalled

--- examples/job_queue/run.sh
+++ examples/job_queue/run.sh
@@ -2,13 +2,18 @@
 # ~root/sky_workdir
 # /var/task/agent_workspace/run.sh
 export HOME=/home/swarms
+unset CONDA_EXE
+unset CONDA_PYTHON_EXE
+export PATH=/var/task/agent_workspace/.venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 if [ ! -f /var/task/agent_workspace/.venv/ ];
 then
    virtualenv /var/task/agent_workspace/.venv/
 fi
 #ls /var/task/agent_workspace/.venv/bin/activate
-. /var/task/agent_workspace/.venv/bin/activateo
-pip install fastapi loguru pydantic uvicorn  termcolor
+. /var/task/agent_workspace/.venv/bin/activate
+
+pip install fastapi   uvicorn  termcolor
 
 # install wr
 pip install -e /opt/swarms/
@@ -19,15 +24,11 @@ pip install -e /opt/swarms/
 cd /var/task/
 pip install -e  /opt/swarms-memory
 
-pip install  pydantic==2.8.2
-
-unset CONDA_EXE
-unset CONDA_PYTHON_EXE
-export HOME=/home/swarms
-export PATH=/var/task/agent_workspace/.venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
 pip install "fastapi[standard]"
+pip install "loguru"
 
-#fastapi /opt/swarms/api/agent_api.py
+pip install  pydantic==2.8.2
+pip freeze
 python /opt/swarms/api/main.py
 #bash
+
