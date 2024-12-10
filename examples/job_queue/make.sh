32e58631174237fa2a3424d42523771f0675a560
mike dupont <mike.dupont@introspector.local>
2024-12-10 14:23:32 -0500

    adding in env file
    removing comments

--- examples/job_queue/make.sh
+++ examples/job_queue/make.sh
@@ -1,22 +1,8 @@
 #!/bin/bash 
-# now convert this into a makefile that tests each size of aws instance types in a structured matrix from small to large, and doing expensive ones after cheaper ones failed.
-# generate a directory structure for the output per type and wish it into existst as dynamic targets
-#
+. .env # load env
 
-# t3.small
-#t2.micro t3.small c5.large T4g.large-Linux
-#m4.xlarge r5.large
-## us-east-1: ami-070b8a957568adf70 # (suse-sle-micro-6-0-byos-v20241204-hvm-ssd-arm64
-
-#us-east-1: ami-0c2a1f58a3414ec50 # (debian-12-amd64-20241201-1948)
-#us-east-2: ami-012d573ea418f6053 # (debian-12-amd64-20241201-1948)
-#us-west-1: ami-0e0fa77836d91d50a # (debian-12-amd64-20241201-1948)
-#us-west-2: ami-087669cf9d44ccdde # (debian-12-amd64-20241201-1948)
-#AMI=ami-0c2a1f58a3414ec50
 AMI=docker:python:latest
 REGION=us-east-1
-
-#
 INSTANCE_SIZES="t3.large"
 for instance_size in $INSTANCE_SIZES;
 do echo
@@ -30,21 +16,12 @@ do echo
        --ports 8000 \
        --region=$REGION \
        --memory=8+ \
+       --env-file .env\
        cluster_docker.yaml
 #                                   GB (e.g., ``--memory=16`` (exactly 16GB),
 #   -s, --detach-setup              If True, run setup in non-interactive mode
-#                                   as part of the job itself. You can safely
-#                                   ctrl-c to detach from logging, and it will
-#                                   not interrupt the setup process. To see the
-#                                   logs again after detaching, use `sky logs`.
-#                                   To cancel setup, cancel the job via `sky
-#                                   cancel`. Useful for long-running setup
-#                                   commands.
 #   -d, --detach-run                If True, as soon as a job is submitted,
-#                                   return from this call and do not stream
-#                                   execution logs.
 #   --docker                        If used, runs locally inside a docker
-#                                   container.
 #   --region TEXT                   The region to use. If specified, overrides
 #   --zone TEXT                     The zone to use. If specified, overrides the
 #   --num-nodes INTEGER             Number of nodes to execute the task on.
@@ -68,8 +45,8 @@ do echo
 #   --fast                          [Experimental] If the cluster is already up
 
    if [ $? -eq 0 ]; then 
-       sky exec swarms job-install-swarms.yaml; 
-       sky exec swarms jobswarms.yaml; 
+       sky exec --env-file .env swarms job-install-swarms.yaml; 
+       sky exec --env-file .env swarms jobswarms.yaml; 
    else 
        echo "Test failed for instance size: $instance_size";
 #       sky down swarms -y
@@ -77,9 +54,3 @@ do echo
    fi;
 #   sky down swarms -y
 done
-
-#launch: $(generate "some how magically we create output/size-${Type}/test-${TestName}/first-report.org targets in a intelligent matrix")
-
-#output/size-${Type}/test-${TestName}/first-report.org
-#	bash ./generate_report.sh ${Type} ${TestName}
-
